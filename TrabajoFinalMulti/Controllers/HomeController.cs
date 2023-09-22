822ecc7f036180c1291a9ab58618ffdd0a48339a
Alonso Cortez <practicas.0061@sesitdigital.com>
2023-09-21 23:09:41 -0500

    register added

--- TrabajoFinalMulti/Controllers/HomeController.cs
+++ TrabajoFinalMulti/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace TrabajoFinalMulti.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
---
+++ TrabajoFinalMulti/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrabajoFinalMulti.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        
+    }
+}
