ffe729cce78fcb76eb30392226fa55a5fb3c6f20
Jose Moscol <JAMoscolRojas@outlook.es>
2023-09-23 00:32:05 -0500

    Creacion de la vista y controlador del administrador

---
+++ TrabajoFinalMulti/Controllers/VistasUsuariosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrabajoFinalMulti.Data;
+using TrabajoFinalMulti.Models;
+using TrabajoFinalMulti.ViewModel;
+
+namespace TrabajoFinalMulti.Controllers
+{
+    public class VistasUsuariosController : Controller
+    {
+        public readonly ApplicationDbContext objUsuario;
+
+        public VistasUsuariosController(ApplicationDbContext dbContext)
+        {
+            objUsuario = dbContext;
+        }
+        public IActionResult VistaAdmin()
+        {
+            List<Estudiante> listaEstudiante = objUsuario.Estudiante.ToList();
+            return View(listaEstudiante);
+        }
+    }
+}
