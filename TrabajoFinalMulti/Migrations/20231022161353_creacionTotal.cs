b90b97d551c874089da1bdffa47d50a870ec6aed
Jose Moscol <JAMoscolRojas@outlook.es>
2023-10-22 18:27:55 -0500

    cambio color verde css

---
+++ TrabajoFinalMulti/Migrations/20231022161353_creacionTotal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrabajoFinalMulti.Migrations
+{
+    /// <inheritdoc />
+    public partial class creacionTotal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
