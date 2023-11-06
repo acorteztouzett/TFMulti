27c03d164ef3918394365bb56a78d7ed8ff9f092
Kike0703 <enriqueazabache07@gmail.com>
2023-11-05 21:02:38 -0500

    Vista Estudiante 3.0

--- TrabajoFinalMulti/Controllers/EstudianteController.cs
+++ TrabajoFinalMulti/Controllers/EstudianteController.cs
@@ -14,15 +14,19 @@ namespace TrabajoFinalMulti.Controllers
         {
             _context = dbContext;
         }
-        
+
         public IActionResult Index()
         {
             var objEstudiante = JsonConvert.DeserializeObject<EstudiantesPorCurso>(HttpContext.Session.GetString("SUsuario"));
-            var cursosEstudiante = _context.Curso.Where(e => e.Aula_Id == objEstudiante.EstudiantesPorCurso_Id).ToList();
+            var cursosEstudiante = _context.Curso.Where(e => e.Aula_Id == objEstudiante.Estudiante_Id).ToList();
             return View(cursosEstudiante);
         }
-        /*        
-        public IActionResult Index(int id)
+        public IActionResult DetallePorCurso(int id)
+        {
+            var curso = _context.Curso.Find(id);
+            return View(curso);
+        }
+        public IActionResult ListaCursos(int id)
         {
             var curso = _context.Curso.Find(id);
             var listaCursos = _context.Sesiones.Where(e => e.Curso_Id == id).ToList();
@@ -33,24 +37,8 @@ namespace TrabajoFinalMulti.Controllers
             };
             Console.WriteLine(listaFinal);
             return View(listaFinal);
-        }*/
-        public IActionResult EditarPerfil(int? id)
-        {
-            if (id == null)
-            {
-                return View();
-            }
-            var estudiante = _context.Estudiante.FirstOrDefault(c => c.Estudiante_Id == id);
-            return View(estudiante);
         }
-
-        public IActionResult VerHorario()
-        {
-            
-            return View();
-        }
-
-        public IActionResult VerEvaluaciones(int id)
+        public IActionResult ListaEvaluaciones(int id)
         {
             var curso = _context.Curso.Find(id);
             var listaEvaluaciones = _context.Evaluaciones.Where(e => e.Curso_Id == id);
@@ -70,19 +58,12 @@ namespace TrabajoFinalMulti.Controllers
 
             return View(listaNotas);
         }
-        public IActionResult VerAnuncio()
+        public IActionResult ListaAnuncioInformativo()
         {
             List<AnuncioInformativo> listaAnuncioInformativo = _context.AnuncioInformativo.ToList();
             return View(listaAnuncioInformativo);
         }
-        
-        public IActionResult SolicitarAsesoria(Asesoria asesoria)
-        {
-            _context.Asesorias.Add(asesoria);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        /*public IActionResult ListaSesiones(int id)
+        public IActionResult ListaSesiones(int id)
         {
             var curso = _context.Curso.Find(id);
             var listaSesiones = _context.Sesiones.Where(e => e.Curso_Id == id).ToList();
@@ -92,15 +73,59 @@ namespace TrabajoFinalMulti.Controllers
                 Sesiones = listaSesiones
             };
             return View(listaFinal);
-        }*/
-
-        /*public IActionResult AsistenciaPorSesion(int id)
+        }
+        public IActionResult AsistenciaPorSesion(int id)
         {
             var listaAsistencia = _context.EstudiantesPorSesions.Where(e => e.Sesion_Id == id).ToList();
             return View(listaAsistencia);
+        }
+        /*[HttpPost]
+        public IActionResult SolicitarAsesoria(Asesoria asesoria)
+        {
+            _context.Asesorias.Add(asesoria);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }*/
 
+        public IActionResult SolicitarAsesoria(int id)
+        {
+            var curso = _context.Curso.Find(id);
+            var listaAsesorias = _context.Asesorias.Where(e => e.Curso_Id == id);
 
+            var listaCompleta = new AsesoriaViewModel()
+            {
+                Curso = curso,
+                Asesorias = listaAsesorias
+            };
+
+            return View(listaCompleta);
+        }
+        public IActionResult CrearAsesoria(int id)
+        {
+            var asesoria = new RegistroAsesoria
+            {
+                Curso_Id = id // Asignar el valor de id al modelo Evaluacion
+            };
+            return View(asesoria);
+        }
+
+        public IActionResult NuevaAsesoria(RegistroAsesoria asesoria)
+        {
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine(asesoria.Curso_Id);
+                Asesoria asesoria1 = new()
+                {
+                    Curso_Id = asesoria.Curso_Id,
+                    Tema = asesoria.Tema,
+                    Fecha = asesoria.Fecha
+                };
+                _context.Asesorias.Add(asesoria1);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("SolicitarAsesoria", new { id = asesoria.Curso_Id });
+        }
+    
     }
 }
-
---
+++ TrabajoFinalMulti/ViewModel/AsesoriaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using TrabajoFinalMulti.Models;
+
+namespace TrabajoFinalMulti.ViewModel
+{
+    public class AsesoriaViewModel
+    {
+        public Curso Curso { get; set; }
+        public IEnumerable<Asesoria> Asesorias { get; set; }
+    }
+        public class RegistroAsesoria
+        {
+            public string Tema { get; set; }
+            public string Fecha { get; set; }
+            public int Curso_Id { get; set; }
+        }
+   }
+
